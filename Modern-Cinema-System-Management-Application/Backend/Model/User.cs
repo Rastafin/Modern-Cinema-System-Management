81848cd1e97de0e4fcc11d090a8580410ccf4fb9
Krzysiu <UL0262204@edu.uni.lodz.pl>
2023-11-23 22:21:09 +0100

    Models added

---
+++ Modern-Cinema-System-Management-Application/Backend/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
